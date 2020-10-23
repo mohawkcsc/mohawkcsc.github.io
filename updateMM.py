61e169c25b3d1ab4259b1c90aa30758066da2aa0
cmckail <mckail96@gmail.com>
2020-10-22 21:04:01 -0400

    Updated Meeting Minutes

--- updateMM.py
+++ updateMM.py
@@ -5,11 +5,13 @@ directory = "assets\meeting_minutes"
 meetingMinutes = list()
 
 for path in pathlib.Path(directory).iterdir():
-    date = str(path).replace('assets\meeting_minutes\\Minutes_-_',"").replace('_'," ").replace(".docx","")
+    filename = str(path).replace('assets\meeting_minutes\\',"")
+    date = str(filename).replace('Minutes_-_',"").replace('_'," ").replace(".docx","")
     meetingMinutes.append({
         "path":str(path),
-        "date": date
+        "date": date,
+        "filename":str(filename)
     })
 with open("assets\json\meetingMinutes.json", 'w+') as fp:
-    fp.write(json.dumps(f"data: '{meetingMinutes}'", indent=4))
+    fp.write(json.dumps(meetingMinutes, indent=4))
     # meetingMinutes.setdefault()
